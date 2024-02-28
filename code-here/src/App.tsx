671d87bd5888dad16e2de36a7cf54a01334abcaa
MrLad01 <ladayodeji@gmail.com>
2024-02-28 18:28:29 +0100

    update

--- code-here/src/App.tsx
+++ code-here/src/App.tsx
@@ -1,11 +1,70 @@
 import "bootstrap/dist/css/bootstrap-grid.min.css";
+import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
+import Accordion from "react-bootstrap/Accordion";
 
 function App() {
   return (
-    <>
+    <Container fluid className="gx-5 gy-5">
       <h1 className="text-blue-500 text-decoration-underline">Hello World</h1>
-      <h2>Hello world</h2>
-    </>
+      <h2 className="btn btn-primary">Hello world</h2>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            What is Frontend Mentor, and how will it help me?
+          </Accordion.Header>
+          <Accordion.Body>
+            Frontend Mentor offers realistic coding challenges to help
+            developers improve their frontend coding skills with projects in
+            HTML, CSS and JavaScript. It's suitable for all levels and ideal for
+            portfolio building.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header> Is Frontend Mentor free?</Accordion.Header>
+          <Accordion.Body>
+            Yes, Frontend Mentor offers both free and premium coding challenges,
+            with the free option providing access to a range of projects
+            suitable for all skill levels.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            {" "}
+            Can I use Frontend Mentor projects in my portfolio?
+          </Accordion.Header>
+          <Accordion.Body>
+            Yes, you can use projects completed on Frontend Mentor in your
+            portfolio. It's an excellent way to showcase your skills to
+            potential employers!
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            {" "}
+            How can I get help if I'm stuck on a Frontend Mentor challenge?
+          </Accordion.Header>
+          <Accordion.Body>
+            The best place to get help is inside Frontend Mentor's Discord
+            community. There's a help channel where you can ask questions and
+            seek support from other community members.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
   );
 }
 
